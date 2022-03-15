b74be4153e01502799e473cacb2f290510c3003b
Devon Carew <devoncarew@gmail.com>
2022-03-15 15:30:28 -0700

    add tests

--- test/output_tester_test.dart
+++ test/output_tester_test.dart
@@ -2,7 +2,34 @@ import 'package:output_tester/output_tester.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
+  test('calculate.1', () {
     expect(calculate(), 42);
+  });
+
+  group('group.a', () {
+    test('calculate.2', () {
+      expect(calculate(), 42);
+    });
+
+    test('calculate.3', () async {
+      await Future.delayed(Duration(seconds: 1));
+
+      expect(calculate(), 42);
+    });
+  });
+
+  group('group.b', () {
+    test('calculate.2', () async {
+      await Future.delayed(Duration(seconds: 1));
+
+      expect(calculate(), 42);
+    });
+
+    test('calculate.3', () async {
+      await Future.delayed(Duration(seconds: 1));
+
+      await Future.delayed(Duration(seconds: 1));
+      expect(calculate(), 42);
+    });
   });
 }
