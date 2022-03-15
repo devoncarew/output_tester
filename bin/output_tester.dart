c21110222bb1b68daf0c449ce367313e0546bf41
Devon Carew <devoncarew@gmail.com>
2022-03-15 15:05:54 -0700

    initial commit

---
+++ bin/output_tester.dart
@@ -0,0 +1,5 @@
+import 'package:output_tester/output_tester.dart' as output_tester;
+
+void main(List<String> arguments) {
+  print('Hello world: ${output_tester.calculate()}!');
+}
---
+++ test/output_tester_test.dart
@@ -0,0 +1,8 @@
+import 'package:output_tester/output_tester.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
