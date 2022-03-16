36f4b653a6f3489680b1c35c6764deb460919542
Devon Carew <devoncarew@gmail.com>
2022-03-15 17:05:16 -0700

    add some sample output

--- bin/output_tester.dart
+++ bin/output_tester.dart
@@ -1,5 +1,70 @@
-import 'package:output_tester/output_tester.dart' as output_tester;
+import 'package:io/ansi.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${output_tester.calculate()}!');
+  printTest(title: 'test/output_tester_test.dart: calculate.1');
+  printTest(
+    title: 'test/output_tester_test.dart: calculate.2',
+    failure: true,
+    details: _stackTrace,
+  );
+
+  printTest(title: 'test/output_tester_test.dart: group.a calculate.3');
+  printTest(title: 'test/output_tester_test.dart: group.a calculate.4');
+
+  printTest(
+    title: 'test/output_tester_test.dart: calculate.2-1',
+    failure: true,
+    details: _stackTrace,
+  );
+
+  printTest(title: 'test/output_tester_test.dart: group.b calculate.3');
+  printTest(
+    title: 'test/output_tester_test.dart: group.b calculate.4',
+    details: _stdout,
+  );
+  printTest(title: 'test/output_tester_test.dart: group.b calculate.5');
+
+  printTest(
+    title: 'test/output_tester_test.dart: calculate.3',
+    failure: true,
+    details: _stackTrace,
+  );
+
+  print('');
+  print('❎ 9 tests run, ❌ 3 failures.');
 }
+
+void printTest({
+  required String title,
+  bool failure = false,
+  String? details,
+}) {
+  // todo:
+  final prefix = details == null ? '' : '::group::';
+
+  // ❌ ❎ ✅ 🛑 ⛔
+  if (failure) {
+    print('$prefix❌ ${styleBold.wrap(title)}');
+  } else {
+    print('$prefix✅ $title');
+  }
+
+  if (details != null) {
+    print(details);
+    print('::endgroup::');
+  }
+}
+
+const String _stackTrace = '''
+  Expected: <41>
+    Actual: <42>
+  
+  package:test_api                    expect
+  test/output_tester_test.dart 50:5   main.<fn>
+  test/output_tester_test.dart 49:23  main.<fn>
+''';
+
+const String _stdout = '''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sagittis dictum justo, vitae congue nisi suscipit eget. Mauris auctor dui urna, ut semper mauris aliquam a. Nullam gravida laoreet feugiat. Nulla vulputate metus vitae libero tempor, at egestas libero sodales. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla facilisi. Vivamus a arcu vel arcu imperdiet imperdiet. Proin pulvinar elit consectetur, dignissim erat eget, vulputate leo. Cras pharetra hendrerit mi, nec congue libero tincidunt nec. Pellentesque eu nisi ac enim imperdiet gravida eget et ligula. Praesent id sem ultricies, rutrum tortor quis, suscipit lectus. Suspendisse congue sed dolor quis imperdiet. Curabitur at augue tellus. Donec ligula sem, euismod sit amet sodales ac, semper cursus metus. Ut in erat facilisis, varius elit at, venenatis ante. Aliquam erat volutpat.
+
+Vestibulum lacinia neque a diam efficitur, non elementum ipsum iaculis. Etiam ut diam nec nisi semper semper. Donec vel nisi semper, auctor tortor eu, venenatis ipsum. Vestibulum nec ante suscipit, vulputate augue vel, dictum felis. Sed ut facilisis sapien. Vestibulum molestie est at ipsum posuere, dictum gravida orci ullamcorper. Sed ut ornare ipsum, id molestie purus. Praesent vitae nisi convallis, vulputate nulla eget, ornare leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras consequat est non nunc tempus pulvinar. Morbi aliquet, odio in pretium fermentum, ex erat consectetur est, id congue mi diam sed neque. Nulla volutpat est eu mi tempor, at tempor nibh auctor. In convallis laoreet diam, vel eleifend arcu venenatis vel. Curabitur et consequat nulla. Sed eu arcu leo. Proin tincidunt sed massa hendrerit venenatis.''';
